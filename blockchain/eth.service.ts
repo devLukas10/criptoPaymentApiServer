706e7693ef84dfac5635db93157fa21b50fb36ef
devLukas10 <devlucasveraz@gmail.com>
2025-07-26 07:37:06 +0100

    add: Add new feature, to ETH

--- blockchain/eth.service.ts
+++ blockchain/eth.service.ts
@@ -9,6 +9,10 @@ const provider = new ethers.JsonRpcProvider(ETH_PROVIDERS.testnet);
 
 export class ETHService{
 
+    private gasLimit: number = 21000;
+    private maxFeePerGas = ethers.parseUnits("30", "gwei");
+    private maxPriorityFeePerGas = ethers.parseUnits("2", "gwei");
+
     async createWallet(): Promise<BlockchainWalletType> {
         try{
             const account = ethers.Wallet.createRandom(provider);
@@ -29,7 +33,35 @@ export class ETHService{
     async getETHBalance(address: string): Promise<number> {
         try{
             const wei = await provider.getBalance(address);
+
             return EtherFormatWei(wei, ethers);
+        } catch (err) {
+            throw err
+        }
+    }
+
+
+
+    async transferETH(privateKey: string, to: string, amount: any): Promise<any> {
+        
+        try {
+            const wallet = new ethers.Wallet(privateKey, provider);
+            /*
+            const signedTx = await wallet.signTransaction({
+                to: to,
+                value: ethers.parseEther(amount),
+                gasLimit: this.gasLimit,
+                maxFeePerGas: this.maxFeePerGas,
+                maxPriorityFeePerGas: this.maxPriorityFeePerGas,
+                nonce: await provider.getTransactionCount(wallet.address),
+                chainId: (await provider.getNetwork()).chainId,
+                type: 2, // EIP-1559
+            })
+
+            const tx = await provider.broadcastTransaction(signedTx);
+            */
+            console.log(wallet.address)
+
         } catch (err) {
             throw err
         }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import { ETHService } from "../blockchain/eth.service";
 
 const eth = new ETHService();
-const address = '0xe9BD31e0a569098Ffcd3608a35a36cAC399ddCF1';
+const address = '0xD7e9A4061451297A431aF1ABd17aF077b99E4eE2';
+const privateKey = "0x292435042c5f78b91901e7a56a1300a1f015201d2c07dbc6275a1f27d676039d";
+
+const to = "0xD7e9A4061451297A431aF1ABd17aF077b99E4eE2";
 
 (async ()=> {
-    let result = await eth.getETHBalance(address);
+    let result = await eth.transferETH(privateKey, to, 0.001);
 
-    console.log(result)
+    //console.log(result)
 })()
