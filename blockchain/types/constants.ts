ac1123390fd65e22e01f8a3f4d5be7ac6d09a5f5
devLukas10 <devlucasveraz@gmail.com>
2025-07-28 01:43:52 +0100

    fit: new chain add polygon...

--- blockchain/types/constants.ts
+++ blockchain/types/constants.ts
@@ -15,3 +15,9 @@ export const ETH_PROVIDERS = "https://sepolia.drpc.org"; // testnet
 export const ETH_USDT_CONTRACT_ADDRESS = "0x7169D38820dfd117C3FA1f22a697dBA58d90BA06"; // testnet
 export const ETH_USDC_CONTRACT_ADDRESS = "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"; // testnet
 export const ETH_EURC_CONTRACT_ADDRESS = "0x08210F9170F89Ab7658F0B5E3fF39b0E03C594D4"; // testnet
+
+/** POLYGON */
+export const POL_PROVIDERS = "https://polygon-amoy.drpc.org"; // testnet
+export const POL_USDT_CONTRACT_ADDRESS = ""; // testnet
+export const POL_USDC_CONTRACT_ADDRESS = ""; // testnet
+export const POL_EURC_CONTRACT_ADDRESS = ""; // testnet
